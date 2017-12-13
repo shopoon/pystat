7b36a3a72e3175836b08dd1c6f2bd1c855d842d4
TM <Takahito.Miyake@ucsf.edu>
2017-12-13 00:29:25 -0800

    カラム作成手順をclass化しました

--- column_matplotlib.py
+++ column_matplotlib.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-data = pd.read_csv("./pokemon_status.csv", encoding="ShiftJIS")
-data_statistics = data.describe()
-bar = plt.bar(range(3),
+#日本語表示のためのフォント指定
+mpl.rcParams["font.family"] = "AppleGothic"
+
+class Column:
+    def show(self, path):
+        data = pd.read_csv(path, encoding="ShiftJIS")
+        data_statistics = data.describe()
+        bar = plt.bar(range(3),
               data_statistics.loc["mean", ["HP", "こうげき", "ぼうぎょ"]],
               yerr=data_statistics.loc["std", ["HP", "こうげき", "ぼうぎょ"]],
               tick_label=["HP", u"こうげき", u"ぼうぎょ"]
               )
-plt.show(bar)
+        plt.show(bar)
+
+if __name__ == "__main__":
+    test = Column()
+    test.show("pokemon_status.csv")
