238bcbc3f6d4fc0c7867f8dc95ec9d8f24120e71
TM <Takahito.Miyake@ucsf.edu>
2018-01-07 16:51:04 -0800

    menubarを作りました（まだ”開く”しか動作しません）

---
+++ wx_matplotlib/event_handler.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from wx_filedialog import *
+from wx_table import *
+from pandas_opendata import *
+from wx_mpl_xy import *
+
+class Open:
+    def __init__(self):
+        path = filedialog()
+        que = Opendata(path.getpath())
+
+        app = wx.App()
+
+        fr1 = TableFrame(None)
+        fr1.fill_in(que)
+        fr1.Show(True)
+
+        fr2 = wx.Frame(None, title='test')
+        panel = XYPanel(fr2)
+        panel.draw(que)
+        fr2.Show()
+
+        app.MainLoop()
